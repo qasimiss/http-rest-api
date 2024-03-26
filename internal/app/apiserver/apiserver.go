24f9226f6944281f34452d4b03f499df87a42592
gvengerov <gvengerov@ozon.ru>
2024-03-26 20:38:08 +0300

    reg and auth

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"net/http"
 
+	"github.com/gorilla/sessions"
 	"github.com/qasimiss/http-rest-api/internal/app/store/sqlstore"
 )
 
@@ -16,8 +17,8 @@ func Start(config *Config) error {
 	defer db.Close()
 
 	store := sqlstore.New(db)
-
-	s := newServer(store)
+	sessionStore := sessions.NewCookieStore([]byte(config.SessionKey))
+	s := newServer(store, sessionStore)
 
 	return http.ListenAndServe(config.BindAddr, s)
 }
